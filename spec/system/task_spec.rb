311bfe7f2dc0286b48702d7fb1e3add5f754542c
Ryogo-Sasaki <burusenpai@gmail.com>
2020-03-26 15:36:30 +0900

    [Fix]letとitの位置関係を修正しました

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
-  let(:project) {create(:project)}
-  let(:task) {(create(:task, project_id: project.id))}
+  let(:project) { create(:project) }
+  let(:task) { (create(:task, project_id: project.id)) }
   describe 'Task一覧' do
     context '正常系' do
       it '一覧ページにアクセスした場合、Taskが表示されること' do
@@ -46,7 +46,7 @@ RSpec.describe 'Task', type: :system do
         visit project_tasks_path(project)
         click_link 'New Task'
         fill_in 'Title', with: 'aaa'
-        click_button'Create Task'
+        click_button 'Create Task'
         expect(page).to have_content('Task was successfully created.')
         expect(current_path).to eq '/projects/1/tasks/1'
       end
@@ -55,7 +55,7 @@ RSpec.describe 'Task', type: :system do
 
   describe 'Task編集' do
     context '正常系' do
-      xit 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
+      it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
         visit edit_project_task_path(project, task)
         fill_in 'Deadline', with: Time.current
@@ -75,32 +75,34 @@ RSpec.describe 'Task', type: :system do
         expect(current_path).to eq project_task_path(project, task)
       end
 
-      xit '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
-        # TODO: FactoryBotのtraitを利用してください
-        let(:task_done) {(create(:task, :finish_yesterday, project_id: project.id))}
-        visit edit_project_task_path(project, task)
-        select 'todo', from: 'Status'
-        click_button 'Update Task'
-        expect(page).to have_content('todo')
-        expect(page).not_to have_content(Time.current.strftime('%Y-%m-%d'))
-        expect(current_path).to eq project_task_path(project, task)
+      context '既にステータスが完了のタスクのステータスを変更した場合' do
+        # TODO: FactoryBotのtrait  を利用してください
+        let(:task_done) { (create(:task, :finish_yesterday, project_id: project.id)) }
+        fit 'Taskの完了日が更新されないこと' do
+          visit edit_project_task_path(project, task)
+          select 'todo', from: 'Status'
+          click_button 'Update Task'
+          expect(page).to have_content('todo')
+          expect(page).not_to have_content(Time.current.strftime('%Y-%m-%d'))
+          expect(current_path).to eq project_task_path(project, task)
+        end
       end
-    end
-  end
 
-  describe 'Task削除' do
-    context '正常系' do
-      # FIXME: テストが失敗するので修正してください
-      it 'Taskが削除されること' do
-        task
-        visit project_tasks_path(project)
-        click_link 'Destroy'
-        page.driver.browser.switch_to.alert.accept
-        expect(find('.task_list')).not_to have_content task.title
-        expect(Task.count).to eq 0
-        expect(current_path).to eq project_tasks_path(project)
+      describe 'Task削除' do
+        context '正常系' do
+          # FIXME: テストが失敗するので修正してください
+          it 'Taskが削除されること' do
+            task
+            visit project_tasks_path(project)
+            click_link 'Destroy'
+            page.driver.browser.switch_to.alert.accept
+            expect(find('.task_list')).not_to have_content task.title
+            expect(Task.count).to eq 0
+            expect(current_path).to eq project_tasks_path(project)
+          end
+        end
       end
     end
   end
-end
+end 
 
